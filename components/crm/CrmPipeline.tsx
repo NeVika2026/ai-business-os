0804edff111a73dda7ff1b113b9c7640566e8698
NeVika2026 <vvikivv300434@gmail.com>
2026-09-19 08:30:06 +0500

    CRM: expose CSV import from pipeline

--- components/crm/CrmPipeline.tsx
+++ components/crm/CrmPipeline.tsx
@@ -141,6 +141,12 @@ export function CrmPipeline({
             >
               + Новый лид
             </button>
+            <Link
+              href="/crm/import"
+              className="rounded-[16px] border border-white/[0.09] bg-white/[0.025] px-5 py-3 text-sm font-black text-white/66"
+            >
+              Импорт CSV
+            </Link>
             <a
               href="/crm/export"
               className="rounded-[16px] border border-white/[0.09] bg-white/[0.025] px-5 py-3 text-sm font-black text-white/66"
